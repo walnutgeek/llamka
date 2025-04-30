0501e5d37c577fe9e86145b74ae4f5aa3b3b3584
Walnut Geek <wg@walnutgeek.com>
2025-04-30 14:27:28 +0300

    ensure proper types

--- tests/test_sqlite.py
+++ tests/test_sqlite.py
@@ -55,10 +55,15 @@ def test_sqlite_db():
 @pytest.mark.debug
 def test_add_attempt():
     with open_sqlite_db(test_db_path) as conn:
-        src = Source(absolute_path=Path("test.txt"))
-        src.save(conn)
+        s = Source(absolute_path=Path("test.txt"))
+        assert s.source_id == -1
+        s.save(conn)
+        assert s.source_id != -1
+        s_loaded = Source.load_by_id(conn, s.source_id)
+        assert s_loaded == s
+        assert isinstance(s_loaded.absolute_path , Path)
         a = VectorizationAttempt(
-            source_id=src.source_id,
+            source_id=s.source_id,
             timestamp=datetime.now(tz=UTC),
             n_chunks=1,
             error=None,
@@ -68,7 +73,7 @@ def test_add_attempt():
         a.save(conn)
         assert a.attempt_id != -1
         b = VectorizationAttempt(
-            source_id=src.source_id,
+            source_id=s.source_id,
             timestamp=datetime.now(tz=UTC),
             n_chunks=1,
             error=None,
@@ -83,7 +88,7 @@ def test_add_attempt():
         b.save(conn)
         c = VectorizationAttempt(
             attempt_id=6,
-            source_id=src.source_id,
+            source_id=s.source_id,
             timestamp=datetime.now(tz=UTC),
             n_chunks=6,
             error=None,
@@ -98,7 +103,10 @@ def test_add_attempt():
         assert a_loaded == a, f"{a_loaded=} != {a=}"
         assert b_loaded == b, f"{b_loaded=} != {b=}"
         assert c_loaded == c, f"{c_loaded=} != {c=}"
-        select = VectorizationAttempt.select(conn, source_id=src.source_id)
+        assert isinstance(a_loaded.timestamp, datetime)
+        assert isinstance(b_loaded.timestamp, datetime)
+        assert isinstance(c_loaded.timestamp, datetime)
+        select = VectorizationAttempt.select(conn, source_id=s.source_id)
         assert len(select) == 3
         assert a in select
         assert b in select
